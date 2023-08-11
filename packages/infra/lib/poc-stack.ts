a63f9ffbdec7f1cf5dfb6964ab28f4de7fa692f9
Alex Bennett <alexandergraybennett@gmail.com>
2023-08-11 13:01:09 -0600

    working with js

---
+++ packages/infra/lib/poc-stack.ts
@@ -0,0 +1,38 @@
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+
+import * as cdk from "aws-cdk-lib";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as s3assets from "aws-cdk-lib/aws-s3-assets";
+import * as path from "path";
+
+export class PocStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const RaIamLambdaRole = new iam.Role(this, "RaLambdaRole", {
+      assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com"),
+    });
+
+    const runtimeCodePath = path.join(__dirname, "../../app/dist/app.zip");
+
+    const runtimeCode = new s3assets.Asset(this, "RaCodeAsset", {
+      path: runtimeCodePath,
+      readers: [RaIamLambdaRole],
+    });
+
+    const bucketName = runtimeCode.bucket.bucketName;
+
+    // console.log("⚡️ bucketName: ", bucketName);
+
+    const lambdaFunction = new lambda.Function(this, "RaLambda", {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: "index.handler",
+      code: lambda.Code.fromBucket(runtimeCode.bucket, runtimeCode.s3ObjectKey),
+      role: RaIamLambdaRole,
+      memorySize: 1024,
+      timeout: cdk.Duration.seconds(30),
+    });
+  }
+}
